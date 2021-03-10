69cbb2ae43fa3e411b024bae47fafe5c3435576f
a-chikara <aiba102chikara@gmail.com>
2021-03-10 19:16:15 +0900

    商品一覧表示機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index
-    @product = Product.all
+    @product = Product.all.order(created_at: :desc)
   end
 
   
