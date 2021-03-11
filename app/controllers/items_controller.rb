c80bd0e12e44ae11aa00b5458bcc312ca8b9752a
a-chikara <aiba102chikara@gmail.com>
2021-03-11 16:26:23 +0900

    商品詳細表示機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+
   private
   
   def product_params
--- app/controllers/products_controller.rb
+++
@@ -1,8 +0,0 @@
-class ProductsController < ApplicationController
-  
-  private
-
-  def product_params
-    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
-  end
-end
